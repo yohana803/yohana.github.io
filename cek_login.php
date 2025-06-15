e7f593e035a7c0eff48311c530747e3eef1d18b5
yohana803 <yohanafransiska.112008@gmail.com>
2025-06-15 11:16:38 +0700

    Add files via upload

--- cek_login.php
+++ cek_login.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 
-// Data login admin (hardcoded)
-$admin_username = 'admin';
+// Data login Namamu (hardcoded)
+$namamu_username = 'Namamu';
 $admin_password = 'admin123'; // password admin
 
 // Ambil data dari form
@@ -10,9 +10,9 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 
 // Cek apakah username dan password cocok
-if ($username === $admin_username && $password === $admin_password) {
-    $_SESSION['admin'] = $username; // Set session admin
-    header('Location: admin_dashboard.php');
+if ($username === $namamu_username && $password === $namamu_password) {
+    $_SESSION['namamu'] = $username; // Set session namamu
+    header('Location: namamu_dashboard.php');
 } else {
     echo "Login gagal! <a href='login.html'>Coba lagi</a>";
 }
